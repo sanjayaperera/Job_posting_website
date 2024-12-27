7e615ea45a0a7f0329afda492863c31ac5558563
Sanjaya Perera <157728209+sanjayaperera@users.noreply.github.com>
2024-12-28 03:08:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Sanjaya from "./Sanjaya";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main className="p-4">
+      <Sanjaya />
+      <Button>click me</Button>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Sanjaya.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sanjaya() {
+  return (
+    <main>
+      <h1 className="text-xl text-red-500">sanjayaa</h1>
+    </main>
+  );
+}
+
+export default Sanjaya;
